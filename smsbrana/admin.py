e4dfbe10397eed8b6ed0a29d6542b15cfde59fd5
vlinhart <vlinhart@vlinhart-desktop.(none)>
2012-08-08 20:45:42 +0200

    sort in admin by created

--- smsbrana/admin.py
+++ smsbrana/admin.py
@@ -6,5 +6,6 @@ from smsbrana.models import SentSms
 class SentSmsAdmin(admin.ModelAdmin):
     list_display = ('phone_number', 'verification_code','sent_date','delivered','delivered_date')
     search_fields = ('phone_number',)
+    ordering = ['-sent_date',]
 
 admin.site.register(SentSms, SentSmsAdmin)
